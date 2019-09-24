f75da42f9c6eba255d9084484cbeff64a0cd648f
8977shiva <b.skreddy1997@gmail.com>
2019-09-25 02:17:10 +0530

    typescript

---
+++ javaScript/test.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var n = "VidyaSagar";
+console.log(n);
+var numb;
+(function (numb) {
+    numb[numb["one"] = 0] = "one";
+    numb[numb["two"] = 1] = "two";
+    numb[numb["three"] = 2] = "three";
+})(numb || (numb = {}));
+var c = numb.one;
+console.log(c);
+//function
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(5, 10));
---
+++ javaScript/test.ts
@@ -0,0 +1,14 @@
+export{}
+let n:string="VidyaSagar";
+
+console.log(n)
+enum  numb{one, two , three}
+ let c:numb = numb.one;
+  console.log(c)
+
+//function
+function add(num1:number, num2){
+    return num1+num2;
+
+}
+console.log(add(5,10))
