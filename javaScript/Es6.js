f52badd006abea2f0196c965add2d45d404100e1
8977shiva <b.skreddy1997@gmail.com>
2019-10-15 22:59:23 +0530

    es6 variables

---
+++ javaScript/Es6.js
@@ -0,0 +1,21 @@
+// "use strict";
+//  function test(){
+//      var num =100;
+//      console.log("value  of num in tes()"+num);{
+//         let num =200
+//         console.log(""+num) }
+//      }
+ 
+//       test()
+
+// var no=13;
+// var no=30;
+// console.log(no);
+
+// let n=18;
+// let n=15;
+// console.log(n);
+
+// const a=10;
+// a=12
+// console.log(a)
