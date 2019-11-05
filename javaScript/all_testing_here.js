6524c05495a28d8b0db36278983ac1eb60b12b5f
8977shiva <b.skreddy1997@gmail.com>
2019-11-06 03:23:44 +0530

    reserve

--- javaScript/all_testing_here.js
+++ javaScript/all_testing_here.js
@@ -1,36 +1,23 @@
-// var findChar = function(str){
-//    var char =[];
-//   var l=0;
-//    for(var i = 0; i < str.length; i++){
-//      if(str.indexOf(str[i]) !== str.lastIndexOf(str[i])){
-//        char[l] = str[i];
-//        l++
-//        break;
-//      }
-//    }
- 
-//    return char;
-//    }
-//    console.log(findChar('abacb'))
+function palindrome(str) {
+    str = str.toLowerCase();
+    var rev = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      rev += str[i];
+    }
+    console.log(rev)
+}
+palindrome('helllo')
 
-// function getUsers(){
-//             fetch('./User.json')
-//             .then((res) => res.json())
-//             .then((data) => {
-//                 let output = '<h2>Users</h2>';
-//                 data.forEach(function(user){
-//                     output += `
-//                     <ul>
-//                         <li>ID: ${user.id}</li>
-//                         <li>Name: ${user.name}</li>
-//                         <li>Email: ${user.name}</li>
-//                         </ul>
-//                         `   
-//                         ;
-//                 });
-//                 document.getElementById('output').innerHTML = output;
-                
-//             })
-//         }
-// getUsers()
 
+
+function  p (s) {
+  r='';
+  
+
+ for (let char of s){
+   // console.log(char);
+    console.log( r=char+r);
+}
+console.log(r);
+}
+p("hello");
--- javaScript/tempCodeRunnerFile.js
+++ javaScript/tempCodeRunnerFile.js
@@ -1,20 +1,11 @@
-
-
-function pangram(str) {
-    var letters = "abcdefghijklmnopqrstuvwxyz"; // define 26 letters
-    var arr = str.split(""); // split string to array
+function  p (s) {
+  r='';
   
-    for (let i = 0; i < arr.length; i++) {
-      // loop through array elements
-      let lower = arr[i].toLowerCase(); // convert to lower case
-      letters = letters.replace(lower, ""); // match array elements with letters
-    }
-  
-    if (letters.length === 0) {
-      // if lenth of letters = 0, it is a pangram, ie: all letters exist in the string
-      return "Input is a pangram";
-    } else {
-      return "Input is not a pangram";
-    }
-  }
-  console.log(pangram("The quick brown fox jumps over the lazy dog"));
+
+ for (let char of s){
+   // console.log(char);
+    console.log( r=char+r);
+}
+console.log(r);
+}
+p("hello");
