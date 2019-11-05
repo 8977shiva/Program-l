2f5a85439e4473ec7428c34b179dd45bf61bfb88
8977shiva <b.skreddy1997@gmail.com>
2019-11-05 22:28:46 +0530

    call stack

---
+++ javaScript/callstack.js
@@ -0,0 +1,15 @@
+function subtractTwo(num) {
+    return num-2;
+    
+}
+
+function calculate() {
+    const sumTotal= 4+5;
+    return subtractTwo(sumTotal);
+    
+}
+calculate()
+
+
+
+
--- javaScript/tempCodeRunnerFile.js
+++ javaScript/tempCodeRunnerFile.js
@@ -1,13 +1,20 @@
-var n1=0;
-var n3
- var n2=1
- var con=10;
- console.log(n1+""+n3)
- for(var i=2;i<con;i++)
- {
-   n3=n2+n1
-   console.log(n3)
-   n1=n2
-   n2=n3
 
- }
+
+function pangram(str) {
+    var letters = "abcdefghijklmnopqrstuvwxyz"; // define 26 letters
+    var arr = str.split(""); // split string to array
+  
+    for (let i = 0; i < arr.length; i++) {
+      // loop through array elements
+      let lower = arr[i].toLowerCase(); // convert to lower case
+      letters = letters.replace(lower, ""); // match array elements with letters
+    }
+  
+    if (letters.length === 0) {
+      // if lenth of letters = 0, it is a pangram, ie: all letters exist in the string
+      return "Input is a pangram";
+    } else {
+      return "Input is not a pangram";
+    }
+  }
+  console.log(pangram("The quick brown fox jumps over the lazy dog"));
