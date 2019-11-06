6d99540a0309a430cbe60160db77aed3c4079550
8977shiva <b.skreddy1997@gmail.com>
2019-11-07 01:42:33 +0530

    creating   restful api

--- javaScript/express.js
+++ javaScript/express.js
@@ -1,16 +1,31 @@
 const express = require("express");
+const bodyParser =require("body-parser");
 const app = express();
 
- app.use((req, res, next)=>{
-      console.log(" i am  middleware");
-      next();
- });
- 
+//  app.use((req, res, next)=>{
+//       console.log(" i am  middleware");
+//       next();
+//  });
+app. use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
 app.get('/', (req,res)=>{
     res.send("helloo")
-})
+});
 
-app.get('/student', (req,res)=>{
-    res.send("hello students ")
-})
+// app.get('/student', (req,res)=>{
+//     res.send("hello students ")
+// });
+
+// app.post('/student', (req,res)=>{
+//     console.log(req.body);
+//      const user={
+//           name:'sally',
+//           hobby:'soccer'
+//      }; 
+
+//     res.send(user);
+
+// })
 app.listen(3000);
---
+++ javaScript/restfullApi.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+
+const app = express ();
+
+ app.use(bodyParser.urlencoded({ extended: false}));
+ app.use(bodyParser.json());
+
+ app.get('/',(req, res)=>{
+      console.log(req.query)
+    //  req.body
+    //  req.header
+    //  req.params
+     res.send("getting root");
+ });
+ 
+ app.listen(3000);
