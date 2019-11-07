9f02951400242a954d77323d1cc41208bdc33c55
8977shiva <b.skreddy1997@gmail.com>
2019-11-08 02:51:46 +0530

    dotenv

--- javaScript/RestfulApi.js
+++ javaScript/RestfulApi.js
@@ -1,1 +1,22 @@
-console.log('hi')
+ const express= require ('express');
+const app = new express();
+ const mongoose = require('mongoose')
+
+//middleware
+ 
+
+
+
+app.get('/', (req,res)=>{
+    res.send('we  are on home');
+});
+
+ app.get('/posts',(req, res)=>{
+     res.send('we are on posts');
+ });
+
+// connect to db
+ mongoose.connect('mongodb+srv://<admin>:<passworddb>@cluster0-gajoo.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true }, ()=>{
+    console.log("db")
+ })
+app.listen(3000)
